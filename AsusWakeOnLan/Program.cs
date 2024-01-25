bb00a885af3a44c8313a4fdb1bf5d41239d0244d
KonkLaw <22473930+KonkLaw@users.noreply.github.com>
2024-01-25 13:03:47 +0100

    refactor

--- AsusWakeOnLan/Program.cs
+++ AsusWakeOnLan/Program.cs
@@ -10,19 +10,7 @@ class Program
 {
     static void Main()
     {
-        ReadConfig(out string rootUrl, out string mac, out string login, out string password);
-        string url = rootUrl + "/Main_Login.asp";
-        const string wolLocalPath = "/Main_WOL_Content.asp";
-
-        const int waitForActiveSeconds = 10;
-        const string singInIdLogin = "login_username";
-        const string sessionIsBusyClass = "nologin-text";
-        const string singInNamePassword = "login_passwd";
-        const string singInIdButtonId = "button";
-        const string macTextBoxName = "destIP";
-        const string wakeButtonId = "cmdBtn";
-        const string logoutScript = "javascript:logout();";
-
+        LoginInfo loginInfo = ReadConfig();
 
         Console.WriteLine("Driver creation");
         var driverService = FirefoxDriverService.CreateDefaultService();
@@ -36,32 +24,7 @@ class Program
         bool wasException = true;
         try
         {
-            Console.WriteLine("Load start page");
-            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            driver.Navigate().GoToUrl(url);
-            Console.WriteLine("Logging in");
-            var webDriverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitForActiveSeconds));
-            IWebElement singInInput = webDriverWait.Until(d =>
-            {
-                ReadOnlyCollection<IWebElement>? elements = d.FindElements(By.ClassName(sessionIsBusyClass));
-                if (elements.Count > 0)
-                    throw new NoLoginException(elements[0].Text);
-                IWebElement? element = d.FindElement(By.Id(singInIdLogin));
-                return element is { Displayed: true, Enabled: true } ? element : null;
-            })!;
-            singInInput.SendKeys(login);
-            driver.FindElement(By.Name(singInNamePassword)).SendKeys(password);
-            driver.FindElement(By.Id(singInIdButtonId)).Click();
-            Console.WriteLine("Waking up");
-            Uri uri = new Uri(driver.Url);
-            url = uri.AbsoluteUri.Substring(0, uri.AbsoluteUri.Length - uri.LocalPath.Length) + wolLocalPath;
-            driver.Navigate().GoToUrl(url);
-            driver.FindElement(By.Name(macTextBoxName)).SendKeys(mac);
-            driver.FindElement(By.Id(wakeButtonId)).Click();
-            Thread.Sleep(1000); // wait for waking up
-            Console.WriteLine("Logging out");
-            driver.ExecuteScript(logoutScript);
-            driver.SwitchTo().Alert().Accept();
+            RunWol(driver, loginInfo);
             wasException = false;
             Console.WriteLine("Success");
         }
@@ -88,15 +51,64 @@ class Program
         }
     }
 
-    private static void ReadConfig(out string rootUrl, out string mac, out string login, out string password)
+    private static void RunWol(WebDriver driver, LoginInfo loginInfo)
+    {
+        string url = loginInfo.RootUrl + "/Main_Login.asp";
+
+        const string wolLocalPath = "/Main_WOL_Content.asp";
+        const int waitForActiveSeconds = 10;
+        const string singInIdLogin = "login_username";
+        const string sessionIsBusyClass = "nologin-text";
+        const string singInNamePassword = "login_passwd";
+        const string singInIdButtonId = "button";
+        const string macTextBoxName = "destIP";
+        const string wakeButtonId = "cmdBtn";
+        const string logoutScript = "javascript:logout();";
+
+        Console.WriteLine("Load start page");
+        //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        driver.Navigate().GoToUrl(url);
+        Console.WriteLine("Logging in");
+        var webDriverWait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitForActiveSeconds));
+        IWebElement singInInput = webDriverWait.Until(d =>
+        {
+            ReadOnlyCollection<IWebElement>? elements = d.FindElements(By.ClassName(sessionIsBusyClass));
+            if (elements.Count > 0)
+                throw new NoLoginException(elements[0].Text);
+
+            elements = d.FindElements(By.Id(singInIdLogin));
+            if (elements.Count == 0)
+                return null;
+            IWebElement element = elements[0];
+            return element is { Displayed: true, Enabled: true } ? element : null;
+        })!;
+        singInInput.SendKeys(loginInfo.Login);
+        driver.FindElement(By.Name(singInNamePassword)).SendKeys(loginInfo.Password);
+        driver.FindElement(By.Id(singInIdButtonId)).Click();
+        Console.WriteLine("Waking up");
+        Uri uri = new Uri(driver.Url);
+        url = uri.AbsoluteUri.Substring(0, uri.AbsoluteUri.Length - uri.LocalPath.Length) + wolLocalPath;
+        driver.Navigate().GoToUrl(url);
+        driver.FindElement(By.Name(macTextBoxName)).SendKeys(loginInfo.Mac);
+        driver.FindElement(By.Id(wakeButtonId)).Click();
+        Thread.Sleep(1000); // wait for waking up
+        Console.WriteLine("Logging out");
+        driver.ExecuteScript(logoutScript);
+        driver.SwitchTo().Alert().Accept();
+    }
+
+    private static LoginInfo ReadConfig()
     {
         string[] settings = File.ReadAllLines("settings.txt");
-        login = Encoding.UTF8.GetString(Convert.FromBase64String(settings[0]));
-        password = Encoding.UTF8.GetString(Convert.FromBase64String(settings[1]));
-        rootUrl = settings[2];
-        mac = settings[3];
+        string login = Encoding.UTF8.GetString(Convert.FromBase64String(settings[0]));
+        string password = Encoding.UTF8.GetString(Convert.FromBase64String(settings[1]));
+        string rootUrl = settings[2];
+        string mac = settings[3];
+        return new LoginInfo(login, password, rootUrl, mac);
     }
 }
+
+record LoginInfo(string Login, string Password, string RootUrl, string Mac);
 
 class NoLoginException : Exception
 {
